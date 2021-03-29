791ef33ca85fa5531f6a0925290e431e84e23461
unknown <calanhaga@aim.com>
2021-03-28 21:17:07 -0400

    AddedFeature component complete

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { reducer } from "./reducers/featuresReducer";
-import { provider } from "react-redux";
+import { Provider } from "react-redux";
 import { createStore } from "redux";
 
 import "bulma/css/bulma.css";
 import "./styles.scss";
 
-const store = createStore(reducers);
+const store = createStore(reducer);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
